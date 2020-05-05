cd92fd516b577daf17ac50251f9d409fe3a988ed
huyle83 <huyle83@g.ucla.edu>
2020-05-05 12:03:30 -0700

    Added script for deploying
    Change-Id: I41d8a86420b3c8df05d6737182631bab027a2080

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Save docker image
+echo "** Saving the current docker image **";
+docker save -o webserver_image webserver;
+
+# Start using Google Cloud and enable some common APIs
+echo "*** Log in ***";
+gcloud init;
+
+echo "** Enable common APIs ***";
+gcloud services enable \
+    cloudbuild.googleapis.com \
+    compute.googleapis.com \
+    containerregistry.googleapis.com \
+    dns.googleapis.com \
+    sourcerepo.googleapis.com;
+
+# Start a build on Cloud Build using docker/cloudbuild.yaml
+echo "** Start a build on Cloud Build **";
+gcloud builds submit --config docker/cloudbuild.yaml .;
+
+# Kill all docker container processes that are running
+echo "** Kill all docker containers that are running **";
+docker kill $(docker ps -a -q);
+
+# Load new docker image
+echo "** Loading new docker image ***";
+docker load -i webserver_image;
+
+# Run the docker image
+echo "*** Run the docker image ***";
+docker run --rm -t -p 8081:8081 webserver;
